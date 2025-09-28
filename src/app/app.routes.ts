c0c8490c01c070aaa912b5afe5e3a2643528c148
goninski <webadmin@goninski.ch>
2025-09-28 20:56:43 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { LegalNotice } from './legal-notice/legal-notice';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'legal-notice', component: LegalNotice },
+    // Redirect any other path to the home page
+    { path: '**', redirectTo: '' }
+];
