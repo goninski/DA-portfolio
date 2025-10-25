cb3019e7f7ed773a46c31515aaabeef3604e4cdc
goninski <webadmin@goninski.ch>
2025-10-25 15:35:06 +0200

    language in local storage

--- src/app/_services/language.service.ts
+++ src/app/_services/language.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Injectable, PLATFORM_ID, inject, signal } from '@angular/core';
 
 export type Language = 'en' | 'de';
 
@@ -7,9 +8,23 @@ export type Language = 'en' | 'de';
 })
 
 export class LanguageService {
+
+  private platformId = inject(PLATFORM_ID);
   lang = signal<Language>('en');
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedLang = localStorage.getItem('lang');
+      if (storedLang && (storedLang === 'en' || storedLang === 'de')) {
+        this.lang.set(storedLang as Language);
+      }
+    }
+  }
 
   toggleLang(): void {
     this.lang.update((current) => (current === 'en' ? 'de' : 'en'));
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('lang', this.lang());
+    }
   }
 }
