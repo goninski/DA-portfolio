7e2cacf7087cbb7010237d3e2f5c9f11180b941c
goninski <webadmin@goninski.ch>
2025-10-27 23:15:08 +0100

    close mobile nav / fixes testimonials

--- src/app/_shared/header/header.ts
+++ src/app/_shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, HostListener, ElementRef, ViewChild, signal } from '@angular/core';
 
 import { LanguageService } from '../../_services/language.service';
 import { TranslatePipe } from '../../_pipes/translate.pipe';
@@ -15,14 +15,33 @@ export class Header {
   data = data;
   languageService = inject(LanguageService);
   isLangDe = computed(() => this.languageService.lang() === 'de');
-  isNavVisible = false;
+  isNavVisible = signal(false);
+
+  @ViewChild('navMenu') navMenu!: ElementRef;
+  @ViewChild('navToggle') navToggle!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.isNavVisible()) {
+      const clickedInsideNav = this.navMenu.nativeElement.contains(event.target);
+      const clickedOnToggle = this.navToggle.nativeElement.contains(event.target);
+
+      if (!clickedInsideNav && !clickedOnToggle) {
+        this.closeNav();
+      }
+    }
+  }
 
   onLangSwitchChange(): void {
     this.languageService.toggleLang();
   }
 
   toggleNav(): void {
-    this.isNavVisible = !this.isNavVisible;
+    this.isNavVisible.update(value => !value);
+  }
+
+  closeNav(): void {
+    this.isNavVisible.set(false);
   }
 
 }
